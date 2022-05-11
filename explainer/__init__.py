8176e8d179013a956e50f93497afe6e24c13e29f
Farouk <farouk.mokhtar@gmail.com>
2022-05-10 17:38:41 -0700

    cleanup

--- explainer/__init__.py
+++ explainer/__init__.py
@@ -1,6 +1,3 @@
 from explainer.lrp import LRP
 from explainer.lrp_captum import LRP_captum
 from explainer.lrp_mlpf import LRP_MLPF
-
-from explainer.lrp_old import LRP_old
-from explainer.lrp_mlpf_old import LRP_MLPF_old
