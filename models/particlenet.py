b74b44c8699afe910435266ae09e5daf28eb07e3
Farouk <farouk.mokhtar@gmail.com>
2022-06-10 13:10:07 +0200

    up

--- models/particlenet.py
+++ models/particlenet.py
@@ -1,3 +1,5 @@
+from torch_geometric.typing import Adj, OptTensor, PairOptTensor, PairTensor
+from typing import Callable, Optional, Union
 from torch_geometric.data import Data, DataListLoader, Batch
 from torch_geometric.loader import DataLoader
 
@@ -35,9 +37,9 @@ import numpy as np
 import torch.nn.functional as F
 
 
-class ParticleNetEdgeNet(nn.Module):
+class EdgeConvBlock(nn.Module):
     def __init__(self, in_size, layer_size):
-        super(ParticleNetEdgeNet, self).__init__()
+        super(EdgeConvBlock, self).__init__()
 
         layers = []
 
@@ -50,13 +52,10 @@ class ParticleNetEdgeNet(nn.Module):
             layers.append(nn.BatchNorm1d(layer_size))
             layers.append(nn.ReLU())
 
-        self.model = nn.Sequential(*layers)
+        self.edge_conv = EdgeConv(nn.Sequential(*layers), aggr="mean")
 
-    def forward(self, x):
-        return self.model(x)
-
-    def __repr__(self):
-        return "{}(nn={})".format(self.__class__.__name__, self.model)
+    def forward(self, x, edge_index):
+        return self.edge_conv(x, edge_index)
 
 
 class ParticleNet(nn.Module):
@@ -66,128 +65,41 @@ class ParticleNet(nn.Module):
         self.num_classes = num_classes
 
         self.k = 16
-        self.num_edge_convs = 3
-        self.kernel_sizes = [64, 128, 256]
+        self.num_edge_conv_blocks = 3
+        self.kernel_sizes = [self.node_feat_size, 64, 128, 256]
+        self.input_sizes = np.cumsum(self.kernel_sizes)
         self.fc_size = 256
         self.dropout = 0.1
 
-        self.edge_nets = nn.ModuleList()
-        self.edge_convs = nn.ModuleList()
+        # define the edgeconvblocks
+        self.edge_conv_blocks = nn.ModuleList()
+        for i in range(0, self.num_edge_conv_blocks):
+            self.edge_conv_blocks.append(EdgeConvBlock(self.input_sizes[i], self.kernel_sizes[i + 1]))
 
-        self.kernel_sizes.insert(0, self.node_feat_size)
-        self.output_sizes = np.cumsum(self.kernel_sizes)
-
-        self.edge_nets.append(ParticleNetEdgeNet(self.node_feat_size, self.kernel_sizes[1]))
-        self.edge_convs.append(EdgeConv(self.edge_nets[-1], aggr="mean"))
-
-        for i in range(1, self.num_edge_convs):
-            # adding kernel sizes because of skip connections
-            self.edge_nets.append(
-                ParticleNetEdgeNet(self.output_sizes[i], self.kernel_sizes[i + 1])
-            )
-            self.edge_convs.append(EdgeConv(self.edge_nets[-1], aggr="mean"))
-
-        self.fc1 = nn.Sequential(nn.Linear(self.output_sizes[-1], self.fc_size))
-
+        # define the fully connected networks (post-edgeconvs)
+        self.fc1 = nn.Sequential(nn.Linear(self.input_sizes[-1], self.fc_size))
         self.dropout_layer = nn.Dropout(p=self.dropout)
-
         self.fc2 = nn.Linear(self.fc_size, self.num_classes)
 
-    def forward(self, batch, ret_activations=False, relu_activations=False):
+    def forward(self, batch, relu_activations=False):
         x = batch.x
         batch = batch.batch
 
-        for i in range(self.num_edge_convs):
+        for i in range(self.num_edge_conv_blocks):
             # using only angular coords for knn in first edgeconv block
             edge_index = (
                 knn_graph(x[:, :2], self.k, batch) if i == 0 else knn_graph(x, self.k, batch)
             )
             x = torch.cat(
-                (self.edge_convs[i](x, edge_index), x), dim=1
-            )  # concatenating with original features i.e. skip connection
+                (self.edge_conv_blocks[i](x, edge_index), x), dim=1
+            )  # concatenating with latent features i.e. skip connections per EdgeConvBlock
 
         x = global_mean_pool(x, batch)
         x = self.fc1(x)
-
-        if ret_activations:
-            if relu_activations:
-                return F.relu(x)
-            else:
-                return x  # for Frechet ParticleNet Distance
-        else:
-            x = self.dropout_layer(F.relu(x))
+        x = self.dropout_layer(F.relu(x))
 
         return self.fc2(x)  # no softmax because pytorch cross entropy loss includes softmax
 
-#
-# batch_size = 100
-# out_neuron = 0
-#
-# if __name__ == "__main__":
-#     # Check if the GPU configuration and define the global base device
-#     if torch.cuda.device_count() > 0:
-#         print(f'Will use {torch.cuda.device_count()} gpu(s)')
-#         print("GPU model:", torch.cuda.get_device_name(0))
-#         device = torch.device('cuda:0')
-#     else:
-#         print('Will use cpu')
-#         device = torch.device('cpu')
-#
-#     # get sample dataset
-#     in_features = 4
-#     dataset = jetnet.datasets.JetNet(jet_type='g')
-#
-#     # load the dataset in a convenient pyg format
-#     dataset_pyg = []
-#     for data in dataset:
-#         d = Data(x=data[0], y=data[1])
-#         dataset_pyg.append(d)
-#
-#     loader = DataLoader(dataset_pyg, batch_size=batch_size, shuffle=False)
-#
-#     # # train sample model
-#     model = ParticleNet(node_feat_size=in_features)
-    # model.train()
-    # quick_train(device, model, loader, epochs=4)
 
-    # Rtensors_list, preds_list, inputs_list = [], [], []
-    #
-    # # get a sample event for lrp testing
-    # for i, event in enumerate(loader):
-    #     print(f'Explaining event # {i}')
-    #     # break it down to a smaller part for lrp (to avoid memory issues)
-    #
-    #     def get_small_batch(event, size):
-    #         small_batch = Batch()
-    #         small_batch.x = event.x[:size]
-    #         small_batch.ygen = event.ygen[:size]
-    #         small_batch.ygen_id = event.ygen_id[:size]
-    #         small_batch.ycand = event.ycand[:size]
-    #         small_batch.ycand_id = event.ycand_id[:size]
-    #         small_batch.batch = event.batch[:size]
-    #         return small_batch
-    #
-    #     event = get_small_batch(event, size=size)
-    #
-    #     # run lrp on sample model
-    #     model.eval()
-    #     lrp_instance = LRP_MLPF(device, model, epsilon=1e-9)
-    #     Rtensor, pred, input = lrp_instance.explain(event, neuron_to_explain=out_neuron)
-    #
-    #     Rtensors_list.append(Rtensor.detach().to('cpu'))
-    #     preds_list.append(pred.detach().to('cpu'))
-    #     inputs_list.append(input.detach().to('cpu').to_dict())
-    #
-    #     # print('Checking conservation of Rscores for a random sample')
-    #     # sample = 26
-    #     # print('R_input ', Rtensor[sample].sum().item())
-    #     # print('R_output', model(small_batch)[0][sample][0].item())
-    #     if i == 2:
-    #         break
-    # with open('Rtensors_list.pkl', 'wb') as f:
-    #     pkl.dump(Rtensors_list, f)
-    # with open('preds_list.pkl', 'wb') as f:
-    #     pkl.dump(preds_list, f)
-    # with open('inputs_list.pkl', 'wb') as f:
-    #     pkl.dump(inputs_list, f)
+# model = ParticleNet(node_feat_size=in_features)
 # model
