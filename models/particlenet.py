e0df7a0879641f2bc76987f03839a7b64e929291
Farouk <farouk.mokhtar@gmail.com>
2022-08-08 16:22:18 +0200

    first iteration

--- models/particlenet.py
+++ models/particlenet.py
@@ -70,6 +70,12 @@ for batch in loader:
 
 
 class EdgeConv_f(MessagePassing):
+    """
+    Edits:
+    1. cat[x_i, x_j - x_i] -> [x_i, x_j]
+    2. retrieve edge_activations
+    """
+
     def __init__(self, nn: Callable, aggr: str = 'max', **kwargs):
         super().__init__(aggr=aggr, **kwargs)
         self.nn = nn
@@ -122,8 +128,8 @@ class ParticleNet(nn.Module):
         self.k = k
         # self.num_edge_conv_blocks = 3
         # self.kernel_sizes = [self.node_feat_size, 64, 128, 256]
-        self.num_edge_conv_blocks = 1
-        self.kernel_sizes = [self.node_feat_size, 64]
+        self.num_edge_conv_blocks = 2
+        self.kernel_sizes = [self.node_feat_size, 64, 128]
         self.input_sizes = np.cumsum(self.kernel_sizes)     # [4, 4+64, 4+64+128, 4+64+128+256]
         self.fc_size = 256
         self.dropout = 0.1
@@ -131,49 +137,43 @@ class ParticleNet(nn.Module):
         # define the edgeconvblocks
         self.edge_conv_blocks = nn.ModuleList()
         for i in range(0, self.num_edge_conv_blocks):
-            self.edge_conv_blocks.append(EdgeConvBlock(self.input_sizes[i], self.kernel_sizes[i + 1]))
+            # self.edge_conv_blocks.append(EdgeConvBlock(self.input_sizes[i], self.kernel_sizes[i + 1]))    # with skip connections
+            self.edge_conv_blocks.append(EdgeConvBlock(self.kernel_sizes[i], self.kernel_sizes[i + 1]))
 
         # define the fully connected networks (post-edgeconvs)
-        # self.fc1 = nn.Sequential(nn.Linear(self.input_sizes[-1], self.fc_size))
+        # self.fc1 = nn.Sequential(nn.Linear(self.input_sizes[-1], self.fc_size)) # if skip connection
+        self.fc1 = nn.Linear(self.kernel_sizes[-1], self.fc_size)  # if no skip connection
         # self.dropout_layer = nn.Dropout(p=self.dropout)
-        # self.fc2 = nn.Linear(self.fc_size, self.num_classes)
-        self.fc2 = nn.Linear(self.input_sizes[-1], self.num_classes)    # if skip connection
-        self.fc2 = nn.Linear(self.kernel_sizes[-1], self.num_classes)    # if no skip connection
-
-        self.kernel_sizes[-1],
+        self.fc2 = nn.Linear(self.fc_size, self.num_classes)
 
     def forward(self, batch, relu_activations=False):
         x = batch.x
         batch = batch.batch
         edge_activations = {}
         edge_block_activations = {}
-        edge_block_activations['input'] = {}
-        edge_block_activations['output'] = {}
         edge_index = {}
 
         for i in range(self.num_edge_conv_blocks):
-            edge_block_activations['input'][f'{i}'] = x
             # using only angular coords for knn in first edgeconv block
-            edge_index['i'] = (
+            edge_index[f'edge_conv_{i}'] = (
                 knn_graph(x[:, :2], self.k, batch) if i == 0 else knn_graph(x, self.k, batch)
             )
 
-            edge_block_activations['output'][f'{i}'], edge_activations[f'edge_conv_{i}'] = self.edge_conv_blocks[i](x, edge_index['i'])
+            edge_block_activations[f'edge_conv_{i}'], edge_activations[f'edge_conv_{i}'] = self.edge_conv_blocks[i](x, edge_index[f'edge_conv_{i}'])
 
             # x = torch.cat(
             #     (edge_block_activations['output'][f'{i}'], x), dim=1
             # )  # concatenating with latent features i.e. skip connections per EdgeConvBlock
 
-            x = edge_block_activations['output'][f'{i}']
+            x = edge_block_activations[f'edge_conv_{i}']
 
         x = global_mean_pool(x, batch)
 
-        # x = self.fc1(x)
+        x = self.fc1(x)
         # x = self.dropout_layer(F.relu(x))
         x = self.fc2(x)
-        # return x, msg  # no softmax because pytorch cross entropy loss includes softmax
 
-        return x, edge_activations, edge_block_activations, edge_index['i']  # no softmax because pytorch cross entropy loss includes softmax
+        return x, edge_activations, edge_block_activations, edge_index  # no softmax because pytorch cross entropy loss includes softmax
 
 
 model = ParticleNet(node_feat_size=in_features)
@@ -184,17 +184,16 @@ ret, edge_activations, edge_block_activations, edge_index = model(small_batch)
 
 
 # TODO
-num_neighbors = 2
-num_nodes = 3
+num_neighbors = 16
+num_nodes = 30
 
 small_batch = Batch(x=batch.x[:num_nodes], y=batch.y[:num_nodes], batch=batch.batch[:num_nodes], ptr=batch.ptr[:num_nodes])
 model = ParticleNet(node_feat_size=in_features, k=num_neighbors)
-model
 
-R_edges, edge_index = LRP(model, small_batch, activations, num_nodes)
+R_tensor, R_edges, edge_index = LRP(model, small_batch, num_nodes)
 
 
-def LRP(model, small_batch, activations, num_nodes):
+def LRP(model, small_batch, num_nodes):
 
     # register hooks
     activations = {}
@@ -204,7 +203,8 @@ def LRP(model, small_batch, activations, num_nodes):
             activations[name] = input[0]
         return hook
 
-    num_convs = 1
+    # unpack the EdgeConv Block module to register appropriate hooks for the children modules
+    num_convs = model.num_edge_conv_blocks
     for name, module in model.named_modules():
         if 'edge_conv' in name:
             if '.edge_conv' not in name:
@@ -227,45 +227,34 @@ def LRP(model, small_batch, activations, num_nodes):
 
     # run LRP
     R_edges = {}
-    for idx in range(model.num_edge_conv_blocks):
-        R_tensor_old = redistribute_across_DNN_layer(ret.detach(), model, activations, 'fc2')
-        print(f'Rscores after DNN_layer {R_tensor_old.sum()}')
-        R_tensor_old = redistribute_across_global_pooling(R_tensor_old, edge_block_activations['output']['0'], num_neighbors)
-        print(f'Rscores after global_pooling {R_tensor_old.sum()}')
-        R, R_edges[f'{idx}'] = redistribute_edge_conv(edge_index, model, idx, R_tensor_old, edge_block_activations, edge_activations, activations, num_nodes, num_neighbors)
-    return R_edges, edge_index
 
-    print(f'Sum of R_scores of the input: {round(R.sum().item(),3)}')
-    return edge_index, R_tensor_old1
+    Rscores = redistribute_across_fc_layer(ret.detach(), model, activations, 'fc2')
+    print(f"Rscores after 'fc2' layer: {Rscores.sum()}")
+    Rscores = redistribute_across_fc_layer(Rscores, model, activations, 'fc1')
+    print(f"Rscores after 'fc1' layer: {Rscores.sum()}")
+    Rscores = redistribute_across_global_pooling(Rscores, edge_block_activations[f'edge_conv_{num_convs-1}'], num_neighbors)
+    print(f'Rscores after global_pooling {Rscores.sum()}')
+
+    for idx in range(num_convs - 1, -1, -1):
+        Rscores, R_edges[f'edge_conv_{idx}'] = redistribute_edge_conv(edge_index[f'edge_conv_{idx}'], model, idx, Rscores, edge_activations, activations, num_nodes, num_neighbors)
+
+    return Rscores, R_connections, edge_index
 
 
-def redistribute_edge_conv(edge_index, model, idx, R_tensor_old, edge_block_activations, edge_activations, activations, num_nodes, num_neighbors):
+def redistribute_edge_conv(edge_index, model, idx, Rscores, edge_activations, activations, num_nodes, num_neighbors):
     """
     Function that redistributes Rscores over an EdgeConv block.
 
     takes R_tensor_old ~ (num_nodes, latent_dim_old)
     and returns R_tensor_new ~ (num_nodes, latent_dim_new)
     """
-    R_edges = redistribute_across_edge_pooling(R_tensor_old, edge_index, edge_activations[f'edge_conv_{idx}'], num_neighbors)
-    print(f'Rscores after edge_pooling {R_edges.sum()}')
+    R_edges = redistribute_across_edge_pooling(Rscores, edge_index, edge_activations[f'edge_conv_{idx}'], num_neighbors)
+    print(f'Rscores after edge_pooling # {idx}: {R_edges.sum()}')
     R_scores = redistribute_across_DNN(R_edges, model, idx, activations)
-    print(f'Rscores after DNN {R_scores.sum()}')
+    print(f'Rscores after DNN # {idx}: {R_scores.sum()}')
     R_scores = redistribute_concat_step(edge_index, R_scores, num_nodes, num_neighbors)
-    print(f'Rscores after concat step {R_scores.sum()}')
+    print(f'Rscores after concat step # {idx}: {R_scores.sum()}')
     return R_scores, R_edges
-
-
-edge_index[1][edge_index[0] == 0]
-
-edge_index[0][edge_index[1] == 0]
-
-
-edge_index
-i = 0
-for neighbor in edge_index[0][edge_index[1] == i]:
-    print(neighbor)
-for neighbor in edge_index[0][edge_index[1] == i]:
-    print(neighbor)
 
 
 def redistribute_concat_step(edge_index, R_old, num_nodes, k):
@@ -285,44 +274,29 @@ def redistribute_concat_step(edge_index, R_old, num_nodes, k):
 
     R_new = torch.zeros([num_nodes, latent_dim_new])
 
-    print('R_old', R_old.shape)
     # loop over nodes
     for i in range(num_nodes):
-        # print(R_new[i])
-        # R_new[i] = 0
-        # loop over neighbors
-        # print('i', i)
+        for num_x, x in enumerate(edge_index[1]):
+            if i == x:
+                R_new[i] += R_old[num_x, :latent_dim_new]
 
-        for j in range(k):
-            # for neighbor in edge_index[0][edge_index[1] == i]:
-            # print('neighbor', neighbor)
-            R_new[i] += R_old[(i * k + j), :latent_dim_new]  # selects the neighbors of the nodes
-        #     R_new[i] += R_old[neighbor, :latent_dim_new]  # selects the neighbors of the nodes
-        #
-        # for node in edge_index[0][edge_index[1] != i]:
-        #     if node == i:
-        #         print('node', node)
-        #         R_new[i] += R_old[node, latent_dim_new:]
-        # print('-------------------------------')
+        for num_x, x in enumerate(edge_index[0]):
+            if i == x:
+                R_new[i] += R_old[num_x, latent_dim_new:]
 
-        # additionally, adds the Rscores of the node itself that has influenced other neighbors
-        for x in range(i, num_nodes * k, k):
-            if x == i * k + k:  # trick to avoid double counting
-                continue
-            R_new[i] += R_old[x, latent_dim_new:]
     return R_new
 
 
-def redistribute_across_DNN(R_tensor_old, model, idx, activations):
+def redistribute_across_DNN(R_old, model, idx, activations):
     """
-    TODO: Function that takes R_tensor_old ~ (num_nodes*k, latent_dim_old)
-    and returns R_tensor_new ~ (num_nodes*k, latent_dim_new)
+    TODO: Function that takes R_old ~ (num_nodes*k, latent_dim_old)
+    and returns R_new ~ (num_nodes*k, latent_dim_new)
 
     Follows simple DNN LRP redistribution
 
     from step 2 to step 3
     """
-    R_tensor_new = R_tensor_old
+    R_new = R_old
     epsilon = 1e-9
 
     # loop over DNN layers
@@ -339,21 +313,21 @@ def redistribute_across_DNN(R_tensor_old, model, idx, activations):
             denominator = torch.matmul(activations[name].detach(), W) + epsilon
 
             # (2) scale the Rscores
-            scaledR = R_tensor_new / denominator
+            scaledR = R_new / denominator
 
             # (3) compute the new Rscores
-            R_tensor_new = torch.matmul(scaledR, torch.transpose(W, 0, 1)) * activations[name].detach()
+            R_new = torch.matmul(scaledR, torch.transpose(W, 0, 1)) * activations[name].detach()
 
-    return R_tensor_new
+    return R_new
 
 
-def redistribute_across_edge_pooling(R_tensor_old, edge_index, edge_activations, k):
+def redistribute_across_edge_pooling(R_old, edge_index, edge_activations, k):
     """
     Function that redistributes Rscores from the nodes over the edges.
 
-    takes R_tensor_old ~ (num_nodes, latent_dim)
+    takes R_old ~ (num_nodes, latent_dim)
     and also edge_activations ~ (num_nodes*k, latent_dim)
-    and returns R_tensor_new ~ (num_nodes*k, latent_dim)
+    and returns R_new ~ (num_nodes*k, latent_dim)
 
     Useful to reditsribute the R_scores backward from the averaging of neighboring edges.
 
@@ -361,10 +335,10 @@ def redistribute_across_edge_pooling(R_tensor_old, edge_index, edge_activations,
     """
     epsilon = 1e-9
 
-    num_nodes = R_tensor_old.shape[0]
-    latent_dim = R_tensor_old.shape[1]
+    num_nodes = R_old.shape[0]
+    latent_dim = R_old.shape[1]
 
-    R_tensor_new = torch.ones([num_nodes * k, latent_dim])
+    R_new = torch.ones([num_nodes * k, latent_dim])
 
     # loop over nodes
     for i in range(num_nodes):
@@ -374,18 +348,18 @@ def redistribute_across_edge_pooling(R_tensor_old, edge_index, edge_activations,
             deno = edge_activations[(i * k):(i * k) + k].sum(axis=0)  # summing the edge_activations node_i (i.e. the edge_activations of the neighboring nodes)
 
             # redistribute the Rscores of node_i according to how activated each edge_activation was (normalized by deno)
-            R_tensor_new[(i * k) + j] = R_tensor_old[i] * edge_activations[(i * k) + j] / (deno + epsilon)
+            R_new[(i * k) + j] = R_old[i] * edge_activations[(i * k) + j] / (deno + epsilon)
 
-    return R_tensor_new
+    return R_new
 
 
-def redistribute_across_global_pooling(R_tensor_old, x, k):
+def redistribute_across_global_pooling(R_old, x, k):
     """
     Function that redistributes Rscores from the whole jet over the nodes.
 
-    takes R_tensor_old ~ (1, latent_dim)
+    takes R_old ~ (1, latent_dim)
     and also takes activations x ~ (num_nodes, latent_dim)
-    and returns R_tensor_new ~ (num_nodes, latent_dim)
+    and returns R_new ~ (num_nodes, latent_dim)
 
     Useful to reditsribute the R_scores backward from the averaging of all nodes.
 
@@ -393,9 +367,9 @@ def redistribute_across_global_pooling(R_tensor_old, x, k):
     """
 
     num_nodes = x.shape[0]
-    latent_dim = R_tensor_old.shape[1]
+    latent_dim = R_old.shape[1]
 
-    R_tensor_new = torch.ones([num_nodes, latent_dim])
+    R_new = torch.ones([num_nodes, latent_dim])
 
     # loop over nodes
     for i in range(num_nodes):
@@ -404,22 +378,22 @@ def redistribute_across_global_pooling(R_tensor_old, x, k):
         deno = x.sum(axis=0)  # summing the activations of all nodes
 
         # redistribute the Rscores of the whole jet over the nodes (normalized by deno)
-        R_tensor_new[i] = R_tensor_old[0] * x[i] / deno
-    return R_tensor_new
+        R_new[i] = R_old[0] * x[i] / deno
+    return R_new
 
 
-def redistribute_across_DNN_layer(R_tensor_old, model, activations, layer_name):
+def redistribute_across_fc_layer(R_old, model, activations, layer_name):
     """
-    Function that takes R_tensor_old ~ (num_nodes, latent_dim_old)
-    and returns R_tensor_new ~ (num_nodes, latent_dim_new)
+    Function that takes R_old ~ (num_nodes, latent_dim_old)
+    and returns R_new ~ (num_nodes, latent_dim_new)
 
     Follows simple DNN LRP redistribution over a given layer.
     """
 
     epsilon = 1e-9
-
     # loop over DNN layers
     layer = name2layer(model, layer_name)
+
     W = layer.weight.detach()  # get weight matrix
     W = torch.transpose(W, 0, 1)    # sanity check of forward pass: (torch.matmul(x, W) + layer.bias) == layer(x)
 
@@ -427,12 +401,12 @@ def redistribute_across_DNN_layer(R_tensor_old, model, activations, layer_name):
     denominator = torch.matmul(activations[layer_name], W) + epsilon
 
     # (2) scale the Rscores
-    scaledR = R_tensor_old / denominator
+    scaledR = R_old / denominator
 
     # (3) compute the new Rscores
-    R_tensor_new = torch.matmul(scaledR, torch.transpose(W, 0, 1)) * activations[layer_name]
+    R_new = torch.matmul(scaledR, torch.transpose(W, 0, 1)) * activations[layer_name]
 
-    return R_tensor_new
+    return R_new
 
 
 def name2layer(model, layer_name):
@@ -442,3 +416,14 @@ def name2layer(model, layer_name):
     for name, module in model.named_modules():
         if layer_name == name:
             return module
+
+
+def edge_activations_to_dict(edge_index, edge_activations):
+    """
+    Given the graph structure (defined by edge_index), and the edge activations tensor (retrived from the EdgeConv)
+    constructs the dictionary of connections
+    """
+    R_connections = {}
+    for i in range(len(edge_index[0])):
+        R_connections[f'{edge_index[1][i]}_{edge_index[0][i]}'] = edge_activations['edge_conv_0'][i]
+    return R_connections
