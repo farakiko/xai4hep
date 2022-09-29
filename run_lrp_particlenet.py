867c39e0b4ef987713c304c67ccabe7268af5de6
Farouk <farouk.mokhtar@gmail.com>
2022-09-29 18:43:57 +0200

    up

--- run_lrp_particlenet.py
+++ run_lrp_particlenet.py
@@ -34,7 +34,7 @@ warnings.filterwarnings("ignore")
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--outpath", type=str, default='./binder/', help="path to the trained model directory")
-parser.add_argument("--model_prefix", type=str, default="ParticleNet_6", help="Which model to load")
+parser.add_argument("--model_prefix", type=str, default="ParticleNet_them12", help="Which model to load")
 parser.add_argument("--dataset", type=str, default='./data/toptagging/', help="path to datafile")
 parser.add_argument("--model", type=int, default=-1, help="model to run Rscores for... -1=trained, x=untrained # x")
 parser.add_argument("--quick", dest='quick', action='store_true')
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     ti = time.time()
     for i, jet in enumerate(loader):
 
-        if i == 10:
+        if i == 100:
             if args.quick:
                 break
 
