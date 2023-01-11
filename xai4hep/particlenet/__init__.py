f88d281002b8a3f375f482d0f031bf8654694cff
Farouk <farouk.mokhtar@gmail.com>
2023-01-11 12:02:45 +0100

    merge particlenet training under one script

--- xai4hep/particlenet/__init__.py
+++ xai4hep/particlenet/__init__.py
@@ -1,7 +1,6 @@
 from particlenet.dataset import TopTaggingDataset
 from particlenet.fastjet_utils import scaling_up
 from particlenet.model import ParticleNet
-from particlenet.training import training_loop
 from particlenet.utils import (
     load_data,
     make_dr_Mij_plots,
