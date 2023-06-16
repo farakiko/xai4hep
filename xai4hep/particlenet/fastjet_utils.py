454d28f97c6d3068f0157dddd25d8d3c84ce188d
Farouk <farouk.mokhtar@gmail.com>
2023-06-16 12:01:08 +0200

    up

--- xai4hep/particlenet/fastjet_utils.py
+++ xai4hep/particlenet/fastjet_utils.py
@@ -205,18 +205,12 @@ def scaling_up(outpath, epoch, N_values=15, N_SUBJETS=3, JET_ALGO="CA", jet_radi
         for N in range(N_values):
             # N=0 doesn't make sense here
             for edge in torch.topk(edge_Rscores, N + 1).indices:
-                print("subjet_idx", subjet_idx)
-                print("edge_index[0]", edge_index[0])
-                print("edge_index[0][edge]", edge_index[0][edge])
-                print(subjet_idx[edge_index[0][edge]])
                 if jet_label == 1:
-                    # if subjet_idx[edge_index[0][edge]] != subjet_idx[edge_index[1][edge]]:
                     if subjet_idx[edge_index[0][edge]] != subjet_idx[edge_index[1][edge]]:
                         top_diff[N] += 1
                     else:
                         top_same[N] += 1
                 else:
-                    # if subjet_idx[edge_index[0][edge]] != subjet_idx[edge_index[1][edge]]:
                     if subjet_idx[edge_index[0][edge]] != subjet_idx[edge_index[1][edge]]:
                         qcd_diff[N] += 1
                     else:
