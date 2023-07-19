80c269fd2da8ab9dd323df82b74e08d6643260e0
Javier Duarte <jduarte@ucsd.edu>
2023-07-18 21:48:26 -0700

    update

--- xai4hep/particlenet/dataset.py
+++ xai4hep/particlenet/dataset.py
@@ -1,5 +1,6 @@
 import os.path as osp
 from glob import glob
+import requests
 
 import torch
 from torch_geometric.data import Data, Dataset
@@ -12,21 +13,21 @@ class TopTaggingDataset(Dataset):
         root (str): path
     """
 
-    def __init__(self, root, mode, transform=None, pre_transform=None):
-        super(TopTaggingDataset, self).__init__(root, transform, pre_transform)
-        self.root = f"{self.root}/{mode}"
-        self._processed_dir = Dataset.processed_dir.fget(self)
+    def __init__(self, root, mode, transform=None, pre_transform=None, pre_filter=None):
         self.mode = mode  # train/val/test
+        super().__init__(root, transform, pre_transform, pre_filter)
 
-    def _download(self):
-        pass
-
-    def _process(self):
-        pass
+    def download(self):
+        raw_file = f"{self.root}/raw/{self.mode}.h5"
+        if not osp.exists(raw_file):
+            print(f"Downloading {raw_file} from Zenodo")
+            url = f"https://zenodo.org/record/2603256/files/{self.mode}.h5?download=1"
+            r = requests.get(url)
+            open(raw_file, "wb").write(r.content)
 
     @property
-    def processed_dir(self):
-        return self._processed_dir
+    def raw_file_names(self):
+        return [f"{self.mode}.h5"]
 
     @property
     def processed_file_names(self):
@@ -39,22 +40,23 @@ class TopTaggingDataset(Dataset):
         Transforms the input variables and stores only the following 7 features per particle
             1. ∆η difference in pseudorapidity between the particle and the jet axis
             2. ∆φ difference in azimuthal angle between the particle and the jet axis
-            3. log pT logarithm of the particle’s pT
-            4. log E logarithm of the particle’s energy
-            5. log pT/pT_jet logarithm of the particle’s pT relative to the jet pT
-            6. log E/E_jet logarithm of the particle’s energy relative to the jet energy
+            3. log pT logarithm of the particle's pT
+            4. log E logarithm of the particle's energy
+            5. log pT/pT_jet logarithm of the particle's pT relative to the jet pT
+            6. log E/E_jet logarithm of the particle's energy relative to the jet energy
             7. ∆R
         """
 
         # https://github.com/jet-universe/particle_transformer/blob/main/utils/convert_top_datasets.py
 
-        import awkward0
+        import awkward as ak
         import numpy as np
         import pandas as pd
-        from uproot3_methods import TLorentzVectorArray
+        import vector
+
+        vector.register_awkward()
 
         df = pd.read_hdf(f"{self.root}/raw/{self.mode}.h5", key="table")
-        # df = dataframe.iloc[start:stop]
 
         def _col_list(prefix, max_particles=200):
             return ["%s_%d" % (prefix, i) for i in range(max_particles)]
@@ -67,24 +69,32 @@ class TopTaggingDataset(Dataset):
         mask = _e > 0
         n_particles = np.sum(mask, axis=1)
 
-        px = awkward0.JaggedArray.fromcounts(n_particles, _px[mask])
-        py = awkward0.JaggedArray.fromcounts(n_particles, _py[mask])
-        pz = awkward0.JaggedArray.fromcounts(n_particles, _pz[mask])
-        energy = awkward0.JaggedArray.fromcounts(n_particles, _e[mask])
+        px = ak.unflatten(_px[mask], n_particles)
+        py = ak.unflatten(_py[mask], n_particles)
+        pz = ak.unflatten(_pz[mask], n_particles)
+        energy = ak.unflatten(_e[mask], n_particles)
 
-        p4 = TLorentzVectorArray.from_cartesian(px, py, pz, energy)
+        p4 = ak.zip(
+            {
+                "px": px,
+                "py": py,
+                "pz": pz,
+                "energy": energy,
+            },
+            with_name="Momentum4D",
+        )
 
-        jet_p4 = p4.sum()
+        jet_p4 = ak.sum(p4, axis=-1)
 
         # outputs
         v = {}
         v["label"] = df["is_signal_new"].values
 
-        v["jet_pt"] = jet_p4.pt
-        v["jet_eta"] = jet_p4.eta
-        v["jet_phi"] = jet_p4.phi
-        v["jet_energy"] = jet_p4.energy
-        v["jet_mass"] = jet_p4.mass
+        v["jet_pt"] = jet_p4.pt.to_numpy()
+        v["jet_eta"] = jet_p4.eta.to_numpy()
+        v["jet_phi"] = jet_p4.phi.to_numpy()
+        v["jet_energy"] = jet_p4.energy.to_numpy()
+        v["jet_mass"] = jet_p4.mass.to_numpy()
         v["jet_nparticles"] = n_particles
 
         v["part_px"] = px
@@ -92,10 +102,8 @@ class TopTaggingDataset(Dataset):
         v["part_pz"] = pz
         v["part_energy"] = energy
 
-        _jet_etasign = np.sign(v["jet_eta"])
-        _jet_etasign[_jet_etasign == 0] = 1
-        v["part_deta"] = (p4.eta - v["jet_eta"]) * _jet_etasign
-        v["part_dphi"] = p4.delta_phi(jet_p4)
+        v["part_deta"] = p4.deltaeta(jet_p4)
+        v["part_dphi"] = p4.deltaphi(jet_p4)
 
         # https://github.com/jet-universe/particle_transformer/blob/main/data/TopLandscape/top_kin.yaml
         part_pt = np.hypot(v["part_px"], v["part_py"])
@@ -112,13 +120,13 @@ class TopTaggingDataset(Dataset):
                 Data(
                     x=torch.cat(
                         [
-                            torch.from_numpy(v["part_deta"][jet_index].reshape(-1, 1)),
-                            torch.from_numpy(v["part_dphi"][jet_index].reshape(-1, 1)),
-                            torch.from_numpy(part_pt_log[jet_index].reshape(-1, 1)),
-                            torch.from_numpy(part_e_log[jet_index].reshape(-1, 1)),
-                            torch.from_numpy(part_logptrel[jet_index].reshape(-1, 1)),
-                            torch.from_numpy(part_logerel[jet_index].reshape(-1, 1)),
-                            torch.from_numpy(part_deltaR[jet_index].reshape(-1, 1)),
+                            torch.from_numpy(v["part_deta"][jet_index].to_numpy().reshape(-1, 1)),
+                            torch.from_numpy(v["part_dphi"][jet_index].to_numpy().reshape(-1, 1)),
+                            torch.from_numpy(part_pt_log[jet_index].to_numpy().reshape(-1, 1)),
+                            torch.from_numpy(part_e_log[jet_index].to_numpy().reshape(-1, 1)),
+                            torch.from_numpy(part_logptrel[jet_index].to_numpy().reshape(-1, 1)),
+                            torch.from_numpy(part_logerel[jet_index].to_numpy().reshape(-1, 1)),
+                            torch.from_numpy(part_deltaR[jet_index].to_numpy().reshape(-1, 1)),
                         ],
                         axis=1,
                     ),
@@ -138,7 +146,7 @@ class TopTaggingDataset(Dataset):
                 c += 1
                 data = []
 
-    def __len__(self):
+    def len(self):
         proc_list = glob(osp.join(self.processed_dir, "*.pt"))
         return len(sorted([processed_path.replace(self.processed_dir, ".") for processed_path in proc_list]))
 
@@ -165,12 +173,12 @@ def parse_args():
 
 if __name__ == "__main__":
     """
-    To process train data: python dataset.py --dataset ../data/toptagging/ --mode train
-    To process val data:   python dataset.py --dataset ../data/toptagging/ --mode val
-    To process test data:  python dataset.py --dataset ../data/toptagging/ --mode test
+    To process train data: python dataset.py --dataset data/toptagging/ --mode train
+    To process val data:   python dataset.py --dataset data/toptagging/ --mode val
+    To process test data:  python dataset.py --dataset data/toptagging/ --mode test
     """
 
     args = parse_args()
 
-    topdataset = TopTaggingDataset(root=args.dataset, mode=args.mode)
+    topdataset = TopTaggingDataset(root=f"{args.dataset}/{args.mode}", mode=args.mode)
     topdataset.prepare_ptfiles()
