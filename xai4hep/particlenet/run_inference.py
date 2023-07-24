04df12a3a4b0e3c67188ef32d1249e17032ff062
Javier Duarte <jduarte@ucsd.edu>
2023-07-24 20:31:39 +0000

    fix utils

--- xai4hep/particlenet/run_inference.py
+++ xai4hep/particlenet/run_inference.py
@@ -9,9 +9,9 @@ import numpy as np
 import torch
 import torch_geometric
 from torch_geometric.loader import DataListLoader, DataLoader
-from utils import load_data, make_roc
 
 from xai4hep.particlenet.model import ParticleNet
+from xai4hep.particlenet.utils import load_data, make_roc
 
 plt.style.use(hep.style.CMS)
 plt.rcParams.update({"font.size": 20})
--- xai4hep/particlenet/run_training.py
+++ xai4hep/particlenet/run_training.py
@@ -13,9 +13,9 @@ import torch
 import torch.nn as nn
 import torch_geometric
 from torch_geometric.loader import DataListLoader, DataLoader
-from utils import load_data, make_roc, save_model
 
 from xai4hep.particlenet.model import ParticleNet
+from xai4hep.particlenet.utils import load_data, make_roc, save_model
 
 plt.style.use(hep.style.CMS)
 plt.rcParams.update({"font.size": 20})
